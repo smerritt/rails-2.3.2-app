dc036320436663ec45e2365505d8a0385674f6fd
Sam Merritt <smerritt@engineyard.com>
2010-04-23 17:57:24 -0700

    more hookery

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,3 @@
 $debug_this_mofo = true
-File.open('/tmp/hookenv', 'w') do |f|
-  f.write(ENV.inspect)
-end
-
-run "touch /tmp/before-migrate-cmd-ran"
+run "env > /tmp/hookenv"
+$debug_this_mofo = false
