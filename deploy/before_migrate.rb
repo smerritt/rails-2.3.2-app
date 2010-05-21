8f96bd1e424bee1230db23837b885f88a67ffb91
Sam Merritt <smerritt@engineyard.com>
2010-05-21 11:17:18 -0700

    Dump @node in before_migrate hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,11 @@
 run "echo $RAILS_ENV >> #{release_path}/before_migrate.log"
 run "env > /tmp/hookenv"
 
+File.open("#{release_path}/before_migrates_node", 'w') do |f|
+  require 'pp'
+  f.write @node.pretty_inspect
+end
+
 on_app_master do
   run "echo 'I am the walrus' >> #{release_path}/walrus.log"
 end
