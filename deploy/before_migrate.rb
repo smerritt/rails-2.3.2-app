a67eff69463751dd66a6be4fcbce518c4784e41e
Sam Merritt <smerritt@engineyard.com>
2010-05-21 11:26:17 -0700

    remove hook test

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,11 +1,6 @@
 run "echo $RAILS_ENV >> #{release_path}/before_migrate.log"
 run "env > /tmp/hookenv"
 
-File.open("#{release_path}/before_migrates_node", 'w') do |f|
-  f.write "#{@node[:instance_role]}\n"
-  f.write "#{@node['instance_role']}\n"
-end
-
 on_app_master do
   run "echo 'I am the walrus' >> #{release_path}/walrus.log"
 end
