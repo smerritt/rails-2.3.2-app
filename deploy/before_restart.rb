02117639b2fc4343e757781db7dd925d35bc5f6f
Sam Merritt <spam@andcheese.org>
2010-06-16 11:45:08 -0700

    Role-specific stuff in deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,8 @@
 run "echo $RAILS_ENV >> #{release_path}/before_restart.log"
-puts "Sleeping a while..."
-sleep 20
-puts "...done!"
+
+on_app_master                { puts "I'm an app master!" }
+on_app_servers               { puts "I'm an app server!" }
+on_db_master                 { puts "I'm a DB master!" }
+on_utility_servers           { puts "I'm a utility server!" }
+on_utility_servers('fluffy') { puts "I'm cute!" }
+on_utility_servers('ugly')   { puts "I'm hideous (and this should never print)!" }
