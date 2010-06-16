3ff78db9ffaa851a0dcf39d5e912b16d4f707d0e
Sam Merritt <spam@andcheese.org>
2010-06-16 12:25:10 -0700

    fix name of method in deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,8 +1,8 @@
 run "echo $RAILS_ENV >> #{release_path}/before_restart.log"
 
-on_app_master                { puts "I'm an app master!" }
-on_app_servers               { puts "I'm an app server!" }
-on_db_master                 { puts "I'm a DB master!" }
-on_utility_servers           { puts "I'm a utility server!" }
-on_utility_servers('fluffy') { puts "I'm cute!" }
-on_utility_servers('ugly')   { puts "I'm hideous (and this should never print)!" }
+on_app_master          { puts "I'm an app master!" }
+on_app_servers         { puts "I'm an app server!" }
+on_db_master           { puts "I'm a DB master!" }
+on_utilities           { puts "I'm a utility server!" }
+on_utilities('fluffy') { puts "I'm cute!" }
+on_utilities('ugly')   { puts "I'm hideous (and this should never print)!" }
